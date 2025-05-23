facddb1c0f161b819516b1758993910824a721e0
MiriamMH98 <97252654+MiriamMH98@users.noreply.github.com>
2025-05-23 14:04:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
+import streamlit as st
+
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == st.secrets["auth"]["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        st.text_input("Contraseña", type="password", on_change=password_entered, key="password")
+        st.error("🔒 Contraseña incorrecta")
+        return False
+    else:
+        return True
+
 from PIL import Image
 
 # Cargar y mostrar el logo
